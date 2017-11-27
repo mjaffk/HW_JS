742f8cd56f43efaefb75157ef630d558c677b6d1
Ekaterina Khorina <Mjaffk@yandex.ru>
2017-11-27 03:50:44 +0300

    Menu like component update (bug amazone menu)

--- 55_MenuConstructor.js
+++ 55_MenuConstructor.js
@@ -96,44 +96,17 @@
                 
                 const DELAY = 100;
     
-                let coolMenuShow = throttle( coolMenuShow, DELAY );
-                let coordinates = calculateCoordinates ( submenu, event );
-                let callElement = findElement(event.target, 'LI', 'UL', 'LI');
-                
-                submenu.addEventListener( 'mousemove', coolMenuShow.bind( this, event, submenu, coordinates, callElement ), false );
-                
-                
-                submenu.addEventListener( 'mouseout', hideSubmenu.bind( null, submenu ), false );
-            }
     
-            /**
-             *
-             * @param {HTMLElement} submenu
-             * @param {Object} event
-             * @return {Object}
-             */
-            function calculateCoordinates ( submenu, event ) {
-                
-                let submenuClientRect = submenu.getBoundingClientRect();
-                
-                let submenuCoordinates = {
-                    leftTop : {
-                        X : submenuClientRect.left,
-                        Y : submenuClientRect.top
-                    },
-                    leftBottom : {
-                        X : submenuClientRect.left,
-                        Y : submenuClientRect.bottom
-                    },
-                };
-                
-                let callCoordinates = {
-                    X : event.clientX,
-                    Y : event.clientY
-                };
-                return {submenuCoordinates, callCoordinates};
+                let coolMenuShowThrottled = throttle( coolMenuShow, DELAY );
+    
+                let coordinates = calculateCoordinates( submenu, event );
+                let callElement = findElement( event.target, 'LI', 'UL', 'LI' );
+                callElement.addEventListener( 'mousemove',
+                    coolMenuShowThrottled.bind( this, event, submenu, coordinates, callElement ), false );
+    
+                submenu.addEventListener( 'mouseout', hideSubmenu.bind( this, submenu ), false );
+    
             }
-            
             /**
              * Check mouse move to submenu
              * @param {HTMLElement} submenu
@@ -141,10 +114,117 @@
              * @param {Object} coordinates
              * @param {HTMLElement} callElement
              */
-            function coolMenuShow( event, submenu, coordinates, callElement ) {
-                timeoutId = setTimeout( () => submenu.hidden = true, 1000 );
+            function coolMenuShow( event, submenu,coordinates, callElement ) {
+        
+                clearTimeout( timeoutId );
+        
+                let currentCoord = {
+                    X : event.clientX,
+                    Y : event.clientY,
+                };
+        
+                if (!isInTriangle( currentCoord, coordinates ) && !isInCallElement( currentCoord, callElement)) {
+                    hideSubmenu( submenu );
+                    return;
+                }
+        
+        
+                coordinates.callCoord.X = currentCoord.X;
+                coordinates.callCoord.Y = currentCoord.Y;
+        
+        
+                timeoutId = setTimeout( hideSubmenu.bind( this, submenu ), 400 );
+        
+                /**
+                 * Check is point in the element
+                 * @param currentCoord
+                 * @param elem
+                 * @return {boolean}
+                 */
+                function isInCallElement( currentCoord, elem ) {
+            
+                    let elemCoord = elem.getBoundingClientRect();
+            
+                    let verticalCondition = currentCoord.Y >= elemCoord.top && currentCoord.Y <= elemCoord.bottom;
+            
+                    let horizontalCondition = currentCoord.X >= elemCoord.left && currentCoord.X <= elemCoord.right;
+            
+                    return verticalCondition && horizontalCondition;
+                }
+        
+                /**
+                 * Check is point in the triangle
+                 * @param currentCoord
+                 * @param submenuCoord
+                 * @param callCoord
+                 * @return {boolean}
+                 */
+                function isInTriangle( currentCoord, { submenuCoord, callCoord } ) {
+            
+                    let ratios = {
+                        bottomLine : calculateRatios( callCoord.X, callCoord.Y, submenuCoord.leftBottom.X,
+                            submenuCoord.leftBottom.Y ),
+                
+                        topLine : calculateRatios( callCoord.X, callCoord.Y, submenuCoord.leftTop.X,
+                            submenuCoord.leftTop.Y ),
+                    };
+            
+                    let inLineSegment = (currentCoord.X >=  callCoord.X) && (currentCoord >= submenuCoord.leftTop.X);
+            
+                    let aboveBottomLine = (ratios.bottomLine.a * currentCoord.X  + ratios.bottomLine.b <= currentCoord.Y);
+            
+                    let belowTopLine = (ratios.topLine.b * currentCoord.X + ratios.topLine.b >= currentCoord.Y);
+            
+                    return inLineSegment && aboveBottomLine && belowTopLine;
+                }
+        
+                /**
+                 * calculate math ratios a and b for line (ax + b = y)
+                 * (x0, y0) and (x1, y1) - coordinates
+                 * @param x0
+                 * @param y0
+                 * @param x1
+                 * @param y1
+                 * @return {{a: number, b: number}}
+                 */
+                function calculateRatios( x0, y0, x1, y1 ) {
+                    return {
+                        a : (y1 - y0) / (x1 - x0),
+                        b : (y0 * x1 - y1 * x0) / (x1 - x0),
+                    };
+                }
+        
             }
-
+    
+            /**
+             *
+             * @param {HTMLElement}submenu
+             * @param {Object} event
+             * @return {Object}
+             */
+            function calculateCoordinates( submenu, event ) {
+                
+                let submenuClientRect = submenu.getBoundingClientRect();
+                
+                return {
+                    submenuCoord : {
+                        leftTop : {
+                            X : submenuClientRect.left,
+                            Y : submenuClientRect.top,
+                        },
+                        leftBottom : {
+                            X : submenuClientRect.left,
+                            Y : submenuClientRect.bottom,
+                        },
+                    },
+                    
+                    callCoord : {
+                        X : event.clientX,
+                        Y : event.clientY,
+                    },
+                };
+            }
+            
             
             /**
              * Hide submenu by event
@@ -153,17 +233,18 @@
             function hideSubmenu( submenu ) {
                 if ( timeoutId ) clearTimeout( timeoutId );
                 submenu.hidden = true;
-                removeListeners( submenu );
+                // removeListeners( submenu );
             }
             
-            /**
-             * remove undesired listeners from submenu
-             * @param {HTMLElement} submenu
-             */
-            function removeListeners( submenu ) {
-                submenu.removeEventListener( 'mouseout', hideSubmenu.bind( null, submenu ), false);
-                submenu.removeEventListener( 'mousemove', coolMenuShow.bind( this, event, submenu, coordinates ), false );
-            }
+            // /**
+            //  * remove undesired listeners from submenu
+            //  * @param {HTMLElement} submenu
+            //  */
+            // function removeListeners( submenu ) {
+            //     submenu.removeEventListener( 'mouseout', hideSubmenu.bind( this, submenu ), false );
+            //     submenu.removeEventListener( 'mousemove',
+            //         coolMenuShowThrottled.bind( this, event, submenu, coordinates, callElement ), false );
+            // }
             
             /**
              * wrapper, make delay between function colling
